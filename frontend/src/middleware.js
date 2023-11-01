c3985033f675732461721fbf0aa4f0bb25634e1a
mcpronovost <mcpronovostkurkiency@gmail.com>
2023-10-31 21:09:05 -0400

    update lang redirection

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,8 +1,13 @@
-import os from "os";
+import { languages, defaultLang } from "@/plugins/i18n/utils.js";
 
 export function onRequest (context, next) {
-
+    // ===---
+    const [, currentLang] = context.url.pathname.split("/");
+    if (currentLang && !(languages.includes(currentLang))) {
+        return context.redirect(`${context.url.origin}/${defaultLang}/`, 303);
+    };
+    // ===---
     context.locals.api = `http://${import.meta.env.BACKEND_IP}:8000`;
-
+    // ===---
     return next();
 };
