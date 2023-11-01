e0810d8f0648e1dbe55219182bdd33be7d041b9c
mcpronovost <mcpronovostkurkiency@gmail.com>
2023-10-31 21:08:08 -0400

    update i18n

---
+++ frontend/src/plugins/i18n/utils.js
@@ -0,0 +1,35 @@
+import msgFr from "@/plugins/i18n/fr.json";
+import msgEn from "@/plugins/i18n/en.json";
+
+export const languages = [
+    "fr", "en"
+]
+
+export const defaultLang = "fr";
+
+export const messages = {
+    fr: msgFr,
+    en: msgEn
+};
+
+export const getLang = (url) => {
+    const [, lang] = url.pathname.split("/");
+    if (lang in languages) return lang;
+    return defaultLang;
+};
+
+export const useTranslation = (lang) => {
+    return function t(key) {
+        return messages[lang][key] || messages[defaultLang][key] || key;
+    };
+};
+
+export const formatDate = (date, lang) => {
+    const options = {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+    if (!date) return new Date().toLocaleDateString(lang, options);
+    return new Date(date).toLocaleDateString(lang, options);
+};
