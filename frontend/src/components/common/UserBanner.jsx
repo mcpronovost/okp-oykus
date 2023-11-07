963557465400064482c9bc0a3298b7526db046af
mcpronovost <mcpronovostkurkiency@gmail.com>
2023-11-06 20:36:26 -0500

    update sidebar

--- frontend/src/components/common/UserBanner.jsx
+++ frontend/src/components/common/UserBanner.jsx
@@ -1,17 +1,30 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const CommonUserBanner = ({ avatar, banner }) => {
+const CommonUserBanner = ({ height, avatar, banner, avatarSize, bannerSize }) => {
   return (
-    <div className="okp-userbanner">
-      <div className="okp-banner">
-        <div className="okp-banner-img" style={{ backgroundImage: banner ? `url('${banner}')` : "none"}}></div>
+    <div className="okp-userbanner" style={{
+      height: `${height || avatarSize+bannerSize}px`
+    }}>
+      <div className="okp-banner" style={{
+        height: `${bannerSize}px`
+      }}>
+        <div className="okp-banner-img" style={{
+          backgroundImage: banner ? `url('${banner}')` : "none"
+        }}></div>
       </div>
-      <div className="okp-avatar">
+      <div className="okp-avatar" style={{
+        width: `${avatarSize}px`,
+        height: `${avatarSize}px`,
+        top: `${bannerSize/2}px`
+      }}>
         {avatar ? (
             <img src={avatar} alt="Avatar" />
           ) : (
-            <div className="okp-icon">
+            <div className="okp-icon" style={{
+              fontSize: `${avatarSize/2}px`,
+              lineHeight: `${avatarSize}px`
+            }}>
               <FontAwesomeIcon icon={faUser} />
             </div>
           )}
--- frontend/src/components/core/Sidebar.jsx
+++ frontend/src/components/core/Sidebar.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { getTranslation, getLang } from "@/plugins/i18n";
 import StoreContext from "@/plugins/store";
 import CommonUserBanner from "@/components/common/UserBanner";
-import imgAvatar from "@/assets/img/oykus.jpg";
+import imgOykus from "@/assets/img/oykus.jpg";
 
 const CoreSidebar = () => {
   const lang = getLang(window.location);
@@ -12,8 +12,9 @@ const CoreSidebar = () => {
   return (
     <>
       <aside id="okp-core-sidebar" className={sidebarOpen ? "okp-open" : ""}>
-        <section>
-          <CommonUserBanner />
+        <section id="okp-sidebar-user-info">
+          <CommonUserBanner height={180} banner={imgOykus} avatarSize={120} bannerSize={72} />
+          <h2>Kamuy Sinen</h2>
         </section>
       </aside>
     </>
