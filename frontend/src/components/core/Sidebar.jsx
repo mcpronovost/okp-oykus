df92c2a9c82114a612e88a3011ef23ece4d37ff5
mcpronovost <mcpronovostkurkiency@gmail.com>
2023-11-09 20:00:18 -0500

    update

--- frontend/src/components/core/Sidebar.jsx
+++ frontend/src/components/core/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { getTranslation, getLang } from "@/plugins/i18n";
 import StoreContext from "@/plugins/store";
+import { Scrollbars } from "rc-scrollbars";
 import CommonUserBanner from "@/components/common/UserBanner";
 import imgOykus from "@/assets/img/oykus.jpg";
 
@@ -10,14 +11,16 @@ const CoreSidebar = () => {
   const { sidebarOpen } = useContext(StoreContext);
 
   return (
-    <>
-      <aside id="okp-core-sidebar" className={sidebarOpen ? "okp-open" : ""}>
-        <section id="okp-sidebar-user-info">
-          <CommonUserBanner height={180} banner={imgOykus} avatarSize={120} bannerSize={72} />
-          <h2>M-C Pronovost</h2>
-        </section>
-      </aside>
-    </>
+    <aside id="okp-core-sidebar" className={sidebarOpen ? "okp-open" : ""}>
+      <Scrollbars>
+        <div id="okp-core-sidebar-wrapper">
+          <section id="okp-sidebar-user-info">
+            <CommonUserBanner height={180} banner={imgOykus} avatarSize={120} bannerSize={72} />
+            <h2>M-C Pronovost</h2>
+          </section>
+        </div>
+      </Scrollbars>
+    </aside>
   )
 };
 
