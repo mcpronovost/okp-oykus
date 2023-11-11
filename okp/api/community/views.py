66560a7add1ab6c88f16d3da4a77ca8e1cc7f0f8
mcpronovost <mcpronovostkurkiency@gmail.com>
2023-11-11 11:46:19 -0500

    update community users list api

--- okp/api/community/views.py
+++ okp/api/community/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import get_user_model
-from rest_framework.response import Response
-from rest_framework.status import HTTP_404_NOT_FOUND
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 
+from okp.api.paginations import (
+    okpPagination
+)
 from okp.api.permissions import (
     okpPermissionReadOnly
 )
@@ -13,22 +14,14 @@ from okp.api.community.serializers import (
 User = get_user_model()
 
 
-class okpCommunityUsersListView(APIView):
+class okpCommunityUsersListView(ListAPIView):
     permission_classes = [okpPermissionReadOnly]
-
-    def get(self, request, fallback=None):
-        if fallback:
-            return Response(status=HTTP_404_NOT_FOUND)
-        users_list = User.objects.filter(
-            is_active=True,
-            profile__isnull=False
-        ).order_by(
-            "-last_login", "-date_joined"
-        )
-        return Response({
-            "valid": True,
-            "users": okpUserSerializer(
-                users_list,
-                many=True
-            ).data,
-        })
+    queryset = User.objects.filter(
+        is_active=True,
+        profile__isnull=False
+    ).order_by(
+        "-last_login", "-date_joined"
+    )
+    serializer_class = okpUserSerializer
+    pagination_class = okpPagination
+    page_size = 1
