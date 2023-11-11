b08fae5510046925d38a63b64505a2b639bf5a14
mcpronovost <mcpronovostkurkiency@gmail.com>
2023-11-11 13:22:52 -0500

    update avatar size validator to 200x200

--- okp/users/validators.py
+++ okp/users/validators.py
@@ -4,5 +4,5 @@ from django.core.files.images import get_image_dimensions
 
 def okpValidatorsAvatar(image):
     image_width, image_height = get_image_dimensions(image)
-    if image_width != 120 or image_height != 120:
-        raise ValidationError("Image sizes needs to be 120x120.")
+    if image_width != 200 or image_height != 200:
+        raise ValidationError("Image sizes needs to be 200x200.")
