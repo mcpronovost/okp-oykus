80105c0b77001f9a41c5400d1a9436b6678aa73a
mcpronovost <mcpronovostkurkiency@gmail.com>
2023-11-12 19:21:35 -0500

    update store

--- frontend/src/plugins/store/index.jsx
+++ frontend/src/plugins/store/index.jsx
@@ -1,18 +1,37 @@
 import { createContext, useState } from "react";
+import { Buffer } from "buffer";
 
 export const setStoreItem = (key, value) => {
-  localStorage.setItem(`okp-oykus-${key}`, JSON.stringify(value));
+  localStorage.setItem(`okp-oykus-${key}`, Buffer.from(JSON.stringify(value)).toString("base64"));
 };
 
 export const getStoreItem = (key) => {
   const storedItem = localStorage.getItem(`okp-oykus-${key}`);
-  return storedItem ? JSON.parse(storedItem) : null;
+  return storedItem ? JSON.parse(Buffer.from(storedItem, "base64")) : null;
+};
+
+export const deleteStoreItem = (key) => {
+  localStorage.removeItem(`okp-oykus-${key}`);
 };
 
 const StoreContext = createContext();
 
 export const StoreProvider = ({ children }) => {
-  const [sidebarOpen, setSidebarOpen] = useState(getStoreItem("sidebar-open"));
+  const [user, setUser] = useState(getStoreItem("user") || null);
+  const [sidebarOpen, setSidebarOpen] = useState(getStoreItem("sidebar-open") || false);
+
+  const setStoreUser = (value) => {
+    setStoreItem("user", value);
+    setUser(user => ({
+      ...user,
+      ...value
+    }));
+  };
+
+  const delStoreUser = () => {
+    deleteStoreItem("user");
+    setUser(null);
+  };
 
   const setStoreSidebarOpen = (value) => {
     setStoreItem("sidebar-open", value);
@@ -21,6 +40,9 @@ export const StoreProvider = ({ children }) => {
 
   return (
     <StoreContext.Provider value={{
+      user,
+      setStoreUser,
+      delStoreUser,
       sidebarOpen,
       setStoreSidebarOpen
     }}>
