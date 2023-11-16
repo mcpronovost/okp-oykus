d47861de310d322bbbfe20d2887bbe0b37fbf9a3
mcpronovost <mcpronovostkurkiency@gmail.com>
2023-11-15 20:43:15 -0500

    update logout

--- frontend/src/components/core/Sidebar.jsx
+++ frontend/src/components/core/Sidebar.jsx
@@ -1,10 +1,14 @@
 import { useContext } from "react";
+import { getTranslation, getLang } from "@/plugins/i18n";
 import StoreContext from "@/plugins/store";
 import { Scrollbars } from "rc-scrollbars";
+import OkpRouteLink from "@/components/common/RouteLink.jsx";
 import OkpUserBanner from "@/components/common/UserBanner";
 import imgOykus from "@/assets/img/oykus.jpg";
 
 const CoreSidebar = () => {
+  const lang = getLang(window.location);
+  const t = getTranslation(lang);
   const { user, sidebarOpen } = useContext(StoreContext);
 
   return (
@@ -16,6 +20,9 @@ const CoreSidebar = () => {
               <section id="okp-sidebar-user-info">
                 <OkpUserBanner height={200} avatar={user.avatar || null} banner={user.avatar || imgOykus} avatarSize={120} bannerSize={120} />
                 <h2>{user.name}</h2>
+              </section>
+              <section id="okp-sidebar-user-menu">
+                <OkpRouteLink route={`/${lang}${t("/auth/logout")}/`} classes="okp-sidebar-btn-logout" children={t("Logout")} />
               </section>
             </div>
           </Scrollbars>
