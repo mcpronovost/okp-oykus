16b48dd41cb0ba6a0ad771cc41af5a1984fc11a3
M-C Pronovost <mcpronovostkurkiency@gmail.com>
2024-07-31 13:36:36 -0400

    clean test

--- frontend/src/components/core/_navbar/menu.tsx
+++ frontend/src/components/core/_navbar/menu.tsx
@@ -47,66 +47,6 @@ export default function NavbarMenu(): ReactNode {
               <span className="okp-menu-link-name">{t("Market")}</span>
             </Link>
           </li>
-          <li>
-            <Link href="/cynosts" className="okp-menu-link">
-              <Orbit size={24} className="okp-menu-link-icon" />
-              <span className="okp-menu-link-name">{t("Cynosts")}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/community" className="okp-menu-link">
-              <UsersRound size={24} className="okp-menu-link-icon" />
-              <span className="okp-menu-link-name">{t("Community")}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/achievements" className="okp-menu-link">
-              <Award size={24} className="okp-menu-link-icon" />
-              <span className="okp-menu-link-name">{t("Achievements")}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/rankings" className="okp-menu-link">
-              <BarChart3 size={24} className="okp-menu-link-icon" />
-              <span className="okp-menu-link-name">{t("Rankings")}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/market" className="okp-menu-link">
-              <ShoppingBasket size={24} className="okp-menu-link-icon" />
-              <span className="okp-menu-link-name">{t("Market")}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/cynosts" className="okp-menu-link">
-              <Orbit size={24} className="okp-menu-link-icon" />
-              <span className="okp-menu-link-name">{t("Cynosts")}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/community" className="okp-menu-link">
-              <UsersRound size={24} className="okp-menu-link-icon" />
-              <span className="okp-menu-link-name">{t("Community")}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/achievements" className="okp-menu-link">
-              <Award size={24} className="okp-menu-link-icon" />
-              <span className="okp-menu-link-name">{t("Achievements")}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/rankings" className="okp-menu-link">
-              <BarChart3 size={24} className="okp-menu-link-icon" />
-              <span className="okp-menu-link-name">{t("Rankings")}</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/market" className="okp-menu-link">
-              <ShoppingBasket size={24} className="okp-menu-link-icon" />
-              <span className="okp-menu-link-name">{t("Market")}</span>
-            </Link>
-          </li>
         </ul>
       </Scrollbars>
     </nav>
