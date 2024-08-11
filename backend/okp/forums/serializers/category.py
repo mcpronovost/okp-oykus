0394647b3bcd22c0b43d386fa371119c5e01292b
M-C Pronovost <mcpronovostkurkiency@gmail.com>
2024-08-10 22:33:20 -0400

    add total topics

--- backend/okp/forums/serializers/category.py
+++ backend/okp/forums/serializers/category.py
@@ -58,6 +58,7 @@ class okpForumSectionSerializer(serializers.ModelSerializer):
             "cover",
             "path",
             "total_messages",
+            "total_topics",
             "last_post"
         ]
 
