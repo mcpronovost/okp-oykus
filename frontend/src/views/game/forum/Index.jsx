706ac05b726a4e46f8e46640f76ed61ceb503c69
M-C Pronovost <mcpronovostkurkiency@gmail.com>
2024-08-10 20:22:54 -0400

    update index loader

--- frontend/src/views/game/forum/Index.jsx
+++ frontend/src/views/game/forum/Index.jsx
@@ -1,52 +1,39 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import { getTrans } from "@/_lib/i18n";
 import { api, getHeaders } from "@/_lib/api";
-import OkpLoading from "@/components/common/Loading";
 import OkpForumCategory from "@/components/forum/Category";
 
-export default function ForumIndexView() {
-  const params = useParams();
+export async function loader({ params }) {
   const t = getTrans();
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasError, setHasError] = useState(false);
-  const [data, setData] = useState(null);
-
-  async function getData () {
-    try {
-      const req = await fetch(`${api}/forum/${params.slug}/index/`, {
-        headers: getHeaders(),
-      });
-      if (!req.ok) {
-        throw req.status;
-      }
-      const response = await req.json();
-      return setData(response);
-    } catch (e) {
-      setHasError("error");
-    } finally {
-      setIsLoading(false);
+  try {
+    const req = await fetch(`${api}/forum/${params.slug}/index/`, {
+      headers: getHeaders(),
+    });
+    if (!req.ok) {
+      throw req.status;
     }
-  };
+    const response = await req.json();
+    return {
+      data: response,
+    };
+  } catch (e) {
+    if (e === 401) throw new Response(t("Unauthorized"), { status: 401 });
+    else throw new Response(t("NotFound"), { status: 404 });
+  }
+}
 
-  useEffect(() => {
-    (async () => {
-      getData();
-    })();
-  }, []);
+export default function ForumIndexView() {
+  const { data } = useLoaderData();
+  const t = getTrans();
 
   return (
     <section className="okp-forum-index">
-      {(!hasError && !isLoading && !!data) ? data.categories.map((category) => {
+      {!!data ? data.categories.map((category) => {
         return (
           <OkpForumCategory key={`forum-category-${category.id}`} category={category} />
         )
-      }) : (!hasError && !isLoading) ? (
+      }): (
         <div>vide</div>
-      ) : (!hasError && isLoading) ? (
-        <OkpLoading />
-      ) : (
-        <div>{hasError}</div>
       )}
     </section>
   );
