24823970f61b27c7b936405bebc0450ba2e387d7
M-C Pronovost <mcpronovostkurkiency@gmail.com>
2024-08-13 10:07:22 -0400

    optimize common header

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,17 +1,24 @@
+import { createElement } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function OkpHeader({ children, h, title, subtitle, href }) {
   const navigate = useNavigate();
 
-  function handleCategoryClick (path) {
+  function handleCategoryClick(path) {
     if (!!href) navigate(path);
-  };
+  }
 
   return (
     <header className="okp-header">
       <div className="okp-header-titles">
-        {!h && <h1 onClick={() => handleCategoryClick(href)} className={`okp-header-title ${!!href ? "okp-link" : ""}`}>{title}</h1>}
-        {h == "2" && <h2 onClick={() => handleCategoryClick(href)} className={`okp-header-title ${!!href ? "okp-link" : ""}`}>{title}</h2>}
+        {createElement(
+          `h${h || "1"}`,
+          {
+            onClick: () => handleCategoryClick(href),
+            className: `okp-header-title ${!!href ? "okp-link" : ""}`,
+          },
+          title
+        )}
         {!!subtitle && <p className="okp-header-subtitle">{subtitle}</p>}
       </div>
       {children}
