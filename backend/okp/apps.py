9352530e4d36b1750109420fb714ce1cddb3bfef
mcpronovost <mcpronovostkurkiency@gmail.com>
2024-10-19 08:07:32 -0400

    clean

--- backend/okp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.admin.apps import AdminConfig
-
-
-class okpAdminConfig(AdminConfig):
-    default_site = "okp.admin.okpAdminSite"
--- backend/okp/games/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class okpGamesConfig(AppConfig):
-    name = "okp.games"
-    label = "okpgames"
-    verbose_name = _("Games")
