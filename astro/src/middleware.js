e2d0e1de74c914a39fe9583c164bd3f4a491a55e
mcpronovost <mcpronovostkurkiency@gmail.com>
2024-10-20 15:03:58 -0400

    add protocol

--- astro/src/middleware.js
+++ astro/src/middleware.js
@@ -3,18 +3,21 @@ export function onRequest({ locals, request }, next) {
   const API_PORT = import.meta.env.API_PORT || process.env.API_PORT;
   const API_ENV = import.meta.env.NODE_ENV || process.env.NODE_ENV;
 
+  const API_PROTOCOL = API_ENV !== "development" ? "https" : "http";
+
   if (
     API_URL !== undefined &&
     API_URL !== "" &&
     API_PORT !== undefined &&
     API_PORT !== ""
   ) {
-    locals.api = API_URL + ":" + API_PORT;
+    locals.api = `${API_PROTOCOL}://${API_URL}:${API_PORT}/api`;
   } else {
-    locals.api = "http://localhost:8010";
+    locals.api = "http://localhost:8010/api";
   }
-  locals.protocol = API_ENV !== "development" ? "https" : "http";
+
   locals.uri = new URL(request.url).pathname;
+  locals.protocol = API_PROTOCOL;
 
   // return a Response or the result of calling `next()`
   return next();
