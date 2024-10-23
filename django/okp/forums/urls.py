b24982eae2cc884401ae3d787294b430e7af1b5a
mcpronovost <mcpronovostkurkiency@gmail.com>
2024-10-22 23:05:23 -0400

    update forum

--- django/okp/forums/urls.py
+++ django/okp/forums/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.utils.translation import gettext_lazy as _
 
-from okp.forums.views import okpForumCategoriesView, okpForumCategoryView
+from okp.forums.views import (
+    okpForumCategoriesView,
+    okpForumCategoryView,
+    okpForumSectionView
+)
 
 urlpatterns = [
     path("<slug:slug>/categories/", okpForumCategoriesView.as_view()),
     path("<slug:slug>/categories/<int:pk>/", okpForumCategoryView.as_view()),
+    path("<slug:slug>/sections/<int:pk>/", okpForumSectionView.as_view()),
 ]
--- django/okp/forums/views.py
+++ django/okp/forums/views.py
@@ -2,10 +2,15 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from okp.forums.models import okpForumCategory
+from okp.forums.models import (
+    okpForumCategory,
+    okpForumSection,
+    okpForumTopic
+)
 from okp.forums.serializers import (
     okpForumsCategoriesSerializer,
     okpForumsCategorySerializer,
+    okpForumSectionSerializer
 )
 
 
@@ -31,3 +36,17 @@ class okpForumCategoryView(APIView):
             return Response(None, status=404)
         category = okpForumsCategorySerializer(queryset).data
         return Response(category)
+
+
+class okpForumSectionView(APIView):
+    authentication_classes = []
+    permission_classes = [AllowAny]
+
+    def get(self, request, format=None, *args, **kwargs):
+        queryset = okpForumSection.objects.filter(
+            pk=kwargs["pk"], game__slug=kwargs["slug"]
+        ).first()
+        if queryset is None:
+            return Response(None, status=404)
+        section = okpForumSectionSerializer(queryset).data
+        return Response(section)
