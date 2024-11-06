04b5389babaa80ce7bd3f33db30e47251f65f42a
mcpronovost <mcpronovostkurkiency@gmail.com>
2024-11-05 20:25:05 -0500

    update admin

--- backend/okp/forums/admin.py
+++ backend/okp/forums/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
 
 from okp.forums.models import (
     okpForumCategory,
@@ -21,7 +22,8 @@ class okpForumCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(okpForumSection)
 class okpForumSectionAdmin(admin.ModelAdmin):
-    list_display = ["name", "category", "game", "total_messages", "total_topics", "sortby"]
+    list_display = ["name", "category", "game",
+                    "total_messages", "total_topics", "sortby"]
     readonly_fields = ["created_at", "updated_at"]
     search_fields = ["name"]
     list_filter = [
@@ -32,23 +34,55 @@ class okpForumSectionAdmin(admin.ModelAdmin):
 
 @admin.register(okpForumTopic)
 class okpForumTopicAdmin(admin.ModelAdmin):
-    list_display = ["name", "last_message", "section", "category", "game", "total_messages"]
-    readonly_fields = ["created_at", "updated_at"]
+    list_display = ["get_title", "user", "character", "last_message", "section",
+                    "category", "game", "total_messages"]
+    readonly_fields = ["get_title", "get_path", "slug",
+                       "total_messages", "created_at", "updated_at"]
     search_fields = ["name"]
     list_filter = [
+        ("user", admin.RelatedOnlyFieldListFilter),
+        ("character", admin.RelatedOnlyFieldListFilter),
         ("game", admin.RelatedOnlyFieldListFilter),
         ("category", admin.RelatedOnlyFieldListFilter),
         ("section", admin.RelatedOnlyFieldListFilter)
     ]
+    fieldsets = [
+        (_("Title"), {"fields": ["name", "slug", "get_path"]}),
+        (_("Author"), {"fields": ["user", "character"]}),
+        (_("Forum"), {"fields": ["game", "category", "section"]}),
+        (_("Statistics"), {"fields": ["total_messages"]}),
+        (_("Important dates"), {"fields": ["created_at", "updated_at"]})
+    ]
+
+    @admin.display(description=_("Title"))
+    def get_title(self, obj):
+        return f"{obj.name[:32]}..." if len(obj.name) > 32 else obj.name
+
+    @admin.display(description=_("Path"))
+    def get_path(self, obj):
+        return format_html("<a href='{url}' target='_blank'>{url}</a>", url=obj.path)
 
 
 @admin.register(okpForumMessage)
 class okpForumMessageAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "topic", "section", "category", "game"]
-    readonly_fields = ["created_at", "updated_at"]
+    list_display = ["__str__", "user", "character",
+                    "get_topic", "section", "category", "game"]
+    readonly_fields = ["get_topic", "created_at", "updated_at"]
     search_fields = ["topic__name"]
     list_filter = [
+        ("user", admin.RelatedOnlyFieldListFilter),
+        ("character", admin.RelatedOnlyFieldListFilter),
         ("game", admin.RelatedOnlyFieldListFilter),
         ("category", admin.RelatedOnlyFieldListFilter),
         ("section", admin.RelatedOnlyFieldListFilter)
     ]
+    fieldsets = [
+        (_("Content"), {"fields": ["topic", "content"]}),
+        (_("Author"), {"fields": ["user", "character"]}),
+        (_("Forum"), {"fields": ["game", "category", "section"]}),
+        (_("Important dates"), {"fields": ["created_at", "updated_at"]})
+    ]
+
+    @admin.display(description=_("Topic"))
+    def get_topic(self, obj):
+        return f"{obj.topic.name[:32]}..." if len(obj.topic.name) > 32 else obj.topic.name
