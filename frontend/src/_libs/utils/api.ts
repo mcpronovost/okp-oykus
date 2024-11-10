36f5644b835572dbb522af6418224536ea63c5f2
mcpronovost <mcpronovostkurkiency@gmail.com>
2024-11-10 11:22:46 -0500

    add api fetch + authApi fetch utils

---
+++ frontend/src/_libs/utils/api.ts
@@ -0,0 +1,29 @@
+const API_URL = import.meta.env.API_URL || "django";
+const API_PORT = import.meta.env.API_PORT || "8000";
+const API_PROTOCOL = import.meta.env.API_ENV === "development" ? "http" : "https";
+const BASE_URL = `${API_PROTOCOL}://${API_URL}:${API_PORT}/api`;
+
+export async function fetchApi<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
+  const url = `${BASE_URL}${endpoint}`;
+  
+  const defaultOptions: RequestInit = {
+    credentials: "include", // for handling cookies
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    const response = await fetch(url, { ...defaultOptions, ...options });
+    if (!response.ok) throw new Error(`API call failed: ${response.statusText}`);
+
+    return await response.json();
+  } catch (error) {
+    console.error("API Error:", error);
+    throw error;
+  }
+}
+
+export const authApi = {
+  getUser: () => fetchApi("/auth/me/"),
+};
