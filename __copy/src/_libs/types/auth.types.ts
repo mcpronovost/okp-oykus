afe87014956722040814dce464eb227591742260
mcpronovost <mcpronovostkurkiency@gmail.com>
2024-11-11 22:01:51 -0500

    clean

--- __copy/src/_libs/types/auth.types.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  id: number;
-  playername: string;
-  abbr: string;
-  avatar: string | null;
-}
--- __copy/src/components/common/NotFound.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function OkpNotFound () {
-  return <div>Not Found</div>;
-}
