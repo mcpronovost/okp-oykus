abbb0c84f58c5b1a74b070f8724f12a2e7b68306
mcpronovost <mcpronovostkurkiency@gmail.com>
2024-11-23 00:41:54 -0500

    init api

--- backend/okp/urls.py
+++ backend/okp/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for okp project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+    path("", include("okp.api.urls")),
+] + i18n_patterns()
+
+# Add static and media files serving
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Admin site customization
+admin.site.site_header = _("Oykus Admin Panel")
+admin.site.site_title = _("Oykus Admin Panel")
+admin.site.index_title = _("Administration")
