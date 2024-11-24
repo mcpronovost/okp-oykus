b78531b29aac81949ad4583dafae3b7aa5617ed5
mcpronovost <mcpronovostkurkiency@gmail.com>
2024-11-23 20:13:12 -0500

    update

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,31 +2,44 @@ import { defineConfig, envField } from "astro/config";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
 import path from "path";
+
 // https://astro.build/config
 export default defineConfig({
     output: "server",
     adapter: node({
-        mode: "standalone"
+        mode: "standalone",
     }),
     server: {
         port: 3000,
-        host: true
+        host: true,
     },
     env: {
-      schema: {
-        VITE_DOMAIN: envField.string({
-          context: "server", // "server" or "client"
-          access: "public", // "public" or "secret"
-          optional: true
-        }),
-      }
+        schema: {
+            VITE_PROTOCOL: envField.string({
+                context: "client", // "server" or "client"
+                access: "public", // "public" or "secret"
+            }),
+            VITE_API: envField.string({
+                context: "client",
+                access: "public",
+            }),
+            VITE_API_VERSION: envField.string({
+                context: "client",
+                access: "public"
+            }),
+        },
     },
     compressHTML: true,
     integrations: [react()],
     vite: {
+        server: {
+            watch: {
+                usePolling: true,
+            },
+        },
         build: {
             minify: "esbuild", // Use esbuild for minification
-            target: "esnext" // Set the target for modern browsers
+            target: "esnext", // Set the target for modern browsers
         },
     },
 });
