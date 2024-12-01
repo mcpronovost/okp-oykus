801114e03dd8c8660aa741f98c4b8c9875c08725
mcpronovost <mcpronovostkurkiency@gmail.com>
2024-12-01 12:20:29 -0500

    fix gameCreate serializer validations

--- backend/okp/api/v1/games/serializers.py
+++ backend/okp/api/v1/games/serializers.py
@@ -1,9 +1,28 @@
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from okp.games.models import OkpGame
 
 
 class OkpGameCreateSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        validators=[
+            UniqueValidator(
+                queryset=OkpGame.objects.all(),
+                message=_("This game name is already taken. Please choose another one.")
+            ),
+            MinLengthValidator(
+                limit_value=1,
+                message=_("Game name must be at least 1 characters long.")
+            ),
+            MaxLengthValidator(
+                limit_value=120,
+                message=_("Game name must be at most 120 characters long.")
+            )
+        ]
+    )
     founder = serializers.HiddenField(default=serializers.CurrentUserDefault())
     owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
