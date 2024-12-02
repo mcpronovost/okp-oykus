2f23d42b10ac9f3469ddc84e0605ea94f18d4e29
mcpronovost <mcpronovostkurkiency@gmail.com>
2024-12-01 23:27:57 -0500

    init games types

---
+++ frontend/src/_services/utils/types/games.ts
@@ -0,0 +1,4 @@
+export type ManagementListGameType = {
+    id: string;
+    name: string;
+};
--- frontend/src/_services/utils/types/index.js
+++ frontend/src/_services/utils/types/index.js
@@ -1,5 +1,6 @@
 export * from "./api";
 export * from "./common";
+export * from "./games";
 export * from "./i18n";
 export * from "./router";
 export * from "./store";
