323eaecaa2887cfd98d6294a2bb23021055321b7
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-02-19 21:26:22 -0500

    Remove old frontend

--- oykus/frontend_old/src/services/i18n/config.ts
+++
@@ -1,7 +0,0 @@
-import type { I18nConfigType } from "@mcpronovost/okp-i18n";
-
-export const I18N_CONFIG: I18nConfigType = {
-  defaultLang: import.meta.env.VITE_LANG,
-  supportedLangs: import.meta.env.VITE_LANGS.split(","),
-  localesPath: "/src/services/i18n/locales",
-};
