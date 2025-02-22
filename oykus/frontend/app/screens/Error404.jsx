3be398747bd6a05a5ec178400ca12bbcfa0794d8
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-02-22 00:05:40 -0500

    Create basic Error404 screen

---
+++ oykus/frontend/app/screens/Error404.jsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { OkpText } from "@/components/common";
+
+export default function Error404() {
+  return (
+    <View>
+      <OkpText>Error 404</OkpText>
+    </View>
+  );
+}
