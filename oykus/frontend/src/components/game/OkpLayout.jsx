8925f8e6a4425b3ac3a0fb98b2bd57a9d983c3c5
M-C Pronovost <mc@nixa.ca>
2025-03-17 16:22:45 -0400

    Add footer to OkpGameLayout

--- oykus/frontend/src/components/game/OkpLayout.jsx
+++ oykus/frontend/src/components/game/OkpLayout.jsx
@@ -1,19 +1,41 @@
 import "@/assets/styles/game/layout.scss";
-// import { OkpError } from "@/components/common";
-import { OkpScrollarea } from "@/components/ui";
+import { useI18n } from "@/services/i18n";
+import { OkpLink, OkpScrollarea } from "@/components/ui";
 
 export default function OkpGameLayout({ children, data }) {
   if (!data) return window.location.reload(true);
 
+  const { t } = useI18n();
+
+  const gameCopyear = (() => {
+    const createdYear = new Date(data.created_at).getFullYear();
+    const updatedYear = new Date(data.updated_at).getFullYear();
+    return createdYear === updatedYear ? createdYear : `${createdYear} - ${updatedYear}`;
+  })();
+
   return (
     <OkpScrollarea id="okp-scrollarea">
       <section className="okp-game">
+        {/* Game Header */}
         <header className="okp-game-header">
           <h1 className="okp-game-header-title">
-          <a href={`/g/${data.slug}/`}>{data.title}</a>
-        </h1>
+            <OkpLink href={`/g/${data.slug}/`}>{data.title}</OkpLink>
+          </h1>
         </header>
+
+        {/* Game Content */}
         {children}
+
+        {/* Game Footer */}
+        <footer className="okp-game-footer">
+          <section>
+            <small>
+              {data.title} &copy; {gameCopyear} {data.owner && (
+                <OkpLink href={`/u/${data.owner.id}-${data.owner.slug}/`}>{data.owner.name}</OkpLink>
+              )}. {t("All rights reserved.")}
+            </small>
+          </section>
+        </footer>
       </section>
     </OkpScrollarea>
   );
