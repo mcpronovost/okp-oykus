f67fffe39a6e750afdc540c8dee566772714a137
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-03-22 11:15:30 -0400

    Refactor OkpScrollarea to conditionally render scrollbars based on enableHorizontal and enableVertical props

--- oykus/frontend/src/components/ui/Scrollarea.jsx
+++ oykus/frontend/src/components/ui/Scrollarea.jsx
@@ -1,24 +1,28 @@
 import "@/assets/styles/ui/scrollarea.scss";
 import { ScrollArea } from "radix-ui";
 
-export default function OkpScrollarea({ children, id = "", className = "" }) {
+export default function OkpScrollarea({ children, enableHorizontal = false, enableVertical = true, id = "", className = "" }) {
   return (
     <ScrollArea.Root id={id} className={`okp-scrollarea ${className}`}>
       <ScrollArea.Viewport className="okp-scrollarea-viewport">
         {children}
       </ScrollArea.Viewport>
-      <ScrollArea.Scrollbar
-        className="okp-scrollarea-scrollbar"
-        orientation="horizontal"
-      >
-        <ScrollArea.Thumb className="okp-scrollarea-thumb" />
-      </ScrollArea.Scrollbar>
-      <ScrollArea.Scrollbar
-        className="okp-scrollarea-scrollbar"
-        orientation="vertical"
-      >
-        <ScrollArea.Thumb className="okp-scrollarea-thumb" />
-      </ScrollArea.Scrollbar>
+      {enableHorizontal && (
+        <ScrollArea.Scrollbar
+          className="okp-scrollarea-scrollbar"
+          orientation="horizontal"
+        >
+          <ScrollArea.Thumb className="okp-scrollarea-thumb" />
+        </ScrollArea.Scrollbar>
+      )}
+      {enableVertical && (
+        <ScrollArea.Scrollbar
+          className="okp-scrollarea-scrollbar"
+          orientation="vertical"
+        >
+          <ScrollArea.Thumb className="okp-scrollarea-thumb" />
+        </ScrollArea.Scrollbar>
+      )}
       <ScrollArea.Corner className="okp-scrollarea-corner" />
     </ScrollArea.Root>
   );
