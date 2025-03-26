06f4c75d2f7fceb237aabc91a7f6c9aa09418ec3
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-03-25 22:16:05 -0400

    Refactor route components to remove lazy loading

--- oykus/frontend/src/services/router/routes/index.jsx
+++ oykus/frontend/src/services/router/routes/index.jsx
@@ -1,29 +1,27 @@
-import { lazy } from "react";
-
 const ROUTES = {
     home: {
-      component: lazy(() => import("@/pages/Home.jsx")),
+      component: () => import("@/pages/Home.jsx"),
       paths: {
         en: "",
         fr: "",
       }
     },
     login: {
-      component: lazy(() => import("@/pages/auth/Login.jsx")),
+      component: () => import("@/pages/auth/Login.jsx"),
       paths: {
         en: "login",
         fr: "connexion",
       }
     },
     logout: {
-      component: lazy(() => import("@/pages/auth/Logout.jsx")),
+      component: () => import("@/pages/auth/Logout.jsx"),
       paths: {
         en: "logout",
         fr: "deconnexion",
       }
     },
     error404: {
-      component: lazy(() => import("@/pages/Error404.jsx")),
+      component: () => import("@/pages/Error404.jsx"),
       paths: {
         en: "404",
         fr: "404",
