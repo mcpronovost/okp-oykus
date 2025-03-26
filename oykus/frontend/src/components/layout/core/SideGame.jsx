e8bfd216bf6d9a5f89a2c9ed253ac51781796bbe
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-03-25 22:18:24 -0400

    Add side game layout styles and integrate side game component into the main layout

--- oykus/frontend/src/components/layout/core/SideGame.jsx
+++ oykus/frontend/src/components/layout/core/SideGame.jsx
@@ -1,12 +1,18 @@
 import { Layout, Tooltip } from "antd";
+import { UsersRound, Orbit } from "lucide-react";
+import { useInitData } from "@/services/initData";
+import { useTranslation } from "@/services/translation";
 import { OkpScrollarea, OkpAvatar, OkpLink } from "@/components/ui";
 import imgPachua from "@/assets/img/pachua.jpg";
 
 export default function OkpSideGame() {
   const { Sider } = Layout;
+  const { t } = useTranslation();
+  const { sideGamesPopular } = useInitData();
 
   return (
     <Sider
+      id="okp-core-side-game"
       theme="light"
       width={64}
       reverseArrow
@@ -21,11 +27,41 @@ export default function OkpSideGame() {
       }}
     >
       <OkpScrollarea>
-        <Tooltip title="Pachu'a Wapi-Qatlaalawsiq" placement="left">
-          <OkpLink href="/w/oykus/characters/01-pachua-wapi-qatlaalawsiq/">
-            <OkpAvatar src={imgPachua} alt="Game" size={48} />
-          </OkpLink>
-        </Tooltip>
+        <ul className="okp-list">
+          <li className="okp-list-header">
+            <UsersRound size={24} aria-label={t("Your Characters")} />
+          </li>
+          <li className="okp-list-item">
+            <Tooltip title={"Pachu'a Wapi-Qatlaalawsiq"} placement="left">
+              <OkpLink href="w/oykus/characters/01-pachua-wapi-qatlaalawsiq">
+                <OkpAvatar src={imgPachua} alt="Game" size={48} />
+              </OkpLink>
+            </Tooltip>
+          </li>
+          <li className="okp-list-item">
+            <Tooltip title="Pachu'a Wapi-Qatlaalawsiq" placement="left">
+              <OkpLink href="w/oykus/characters/01-pachua-wapi-qatlaalawsiq">
+                <OkpAvatar src={imgPachua} alt="Game" size={48} />
+              </OkpLink>
+            </Tooltip>
+          </li>
+        </ul>
+        {sideGamesPopular && (
+          <ul className="okp-list">
+            <li className="okp-list-header">
+              <Orbit size={24} aria-label={t("Popular Games")} />
+            </li>
+            {sideGamesPopular.map((game) => (
+              <li className="okp-list-item" key={game.id}>
+                <Tooltip title={game.title} placement="left">
+                  <OkpLink href={`g/${game.slug}`}>
+                    <OkpAvatar src={game.image} fallback={game.abbr} color={game.primary} alt="Game" size={48} />
+                  </OkpLink>
+                </Tooltip>
+              </li>
+            ))}
+          </ul>
+        )}
       </OkpScrollarea>
     </Sider>
   );
