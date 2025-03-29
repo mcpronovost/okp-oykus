75f391289f399d275e4d96baea7d1012b20eb589
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-03-28 21:56:49 -0400

    Fix inputType text

--- oykus/frontend/src/components/form/Field.jsx
+++ oykus/frontend/src/components/form/Field.jsx
@@ -3,7 +3,7 @@ import { Form, Input, Select  } from "antd";
 export default function OkpField({ name, label, placeholder, inputType = "input", options = [], rows = 4, required = false, children, ...props }) {
   return (
     <Form.Item name={name} label={label} rules={[{ required: required }]} hasFeedback>
-      {inputType === "input" ? (
+      {inputType === "text" ? (
         <Input placeholder={placeholder} autoComplete="off" />
       ) : inputType === "textarea" ? (
         <Input.TextArea placeholder={placeholder} rows={rows} autoComplete="off" />
