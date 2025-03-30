ed6ae0883f6cdb6ac230ee11622a7cbcb548b0f6
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-03-30 00:01:04 -0400

    Clean

--- oykus/frontend/src/components/ui/Link.jsx
+++ oykus/frontend/src/components/ui/Link.jsx
@@ -3,7 +3,7 @@ import { useRouter } from "@/services/router";
 
 export default function OkpLink({ children, href, colour, toLang, disabled = false, ...props }) {
   const { r } = useRouter();
-  
+
   const toRouteName = href.startsWith("http") ? href : useMemo(() => r(href, toLang), [href, r, toLang]);
   const linkStyle = useMemo(() => ({
     color: colour || null,
