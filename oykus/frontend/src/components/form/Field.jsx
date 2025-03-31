8d9d9090682de5097142236c2ef2c8ee8eb34c22
mcpronovost <mcpronovostkurkiency@gmail.com>
2025-03-30 22:25:28 -0400

    Add support for additional input types in OkpField component
    - Enhanced OkpField to handle checkbox and upload input types.
    - Introduced maxLength and showCount props for text and textarea inputs.
    - Added disabled prop for all input types.
    - Implemented getValueFromEvent function for handling file uploads.

--- oykus/frontend/src/components/form/Field.jsx
+++ oykus/frontend/src/components/form/Field.jsx
@@ -1,15 +1,81 @@
-import { Form, Input, Select  } from "antd";
+import { Form, Input, Select, Checkbox, Upload } from "antd";
 
-export default function OkpField({ name, label, placeholder, inputType = "input", options = [], rows = 4, size = "default", required = false, hasFeedback = true, children, ...props }) {
+const getValueFromEvent = e => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e === null || e === void 0 ? void 0 : e.fileList;
+};
+
+export default function OkpField({
+  name,
+  label,
+  placeholder,
+  maxLength,
+  showCount = false,
+  inputType = "input",
+  options = [],
+  rows = 4,
+  size = "default",
+  required = false,
+  disabled = false,
+  hasFeedback = true,
+  children,
+  style,
+  ...props
+}) {
   return (
-    <Form.Item name={name} label={label} rules={[{ required: required }]} hasFeedback={hasFeedback}>
+    <Form.Item
+      name={name}
+      label={label}
+      rules={[{ required: required }]}
+      hasFeedback={hasFeedback}
+      valuePropName={
+        inputType === "checkbox"
+          ? "checked"
+          : inputType === "upload"
+          ? "fileList"
+          : "value"
+      }
+      getValueFromEvent={inputType === "upload" ? getValueFromEvent : undefined}
+      style={style}
+    >
       {inputType === "text" ? (
-        <Input placeholder={placeholder} autoComplete="off" size={size} />
+        <Input
+          placeholder={placeholder}
+          autoComplete="off"
+          size={size}
+          maxLength={maxLength}
+          showCount={showCount}
+          disabled={disabled}
+        />
       ) : inputType === "textarea" ? (
-        <Input.TextArea placeholder={placeholder} rows={rows} autoComplete="off" size={size} />
+        <Input.TextArea
+          placeholder={placeholder}
+          rows={rows}
+          autoComplete="off"
+          size={size}
+          maxLength={maxLength}
+          showCount={showCount}
+          disabled={disabled}
+        />
       ) : inputType === "select" ? (
-        <Select placeholder={placeholder} options={options} autoComplete="off" size={size} />
-      ) : children}
+        <Select
+          placeholder={placeholder}
+          options={options}
+          autoComplete="off"
+          size={size}
+          disabled={disabled}
+        />
+      ) : inputType === "checkbox" ? (
+        <Checkbox size={size} disabled={disabled} required={required} />
+      ) : inputType === "upload" ? (
+        <Upload maxLength={1} {...props}>
+          {children}
+        </Upload>
+      ) : (
+        children
+      )}
     </Form.Item>
   );
 }
